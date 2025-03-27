f4648d65226655dac3fabee54c3a8377d3da6577
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-03-27 10:48:37 +0100

    started adding functions to change between questions

---
+++ js/main.js
@@ -0,0 +1,74 @@
+
+// DOM selectors
+questions = document.querySelectorAll('.question')
+startQuizButton = document.getElementById('intro-continue')
+
+// initialize current question counter
+let currentQuestionIndex = 0
+
+// function to set the active question
+const setActiveQuestion = (index) => {
+  // show the active question - hide the others
+  questions.forEach((question, i) => {
+    if (i === index) {
+      question.style.display = 'block'
+    } else {
+      question.style.display = 'none'
+    }
+  });
+}
+
+// function to start Quiz
+const startQuiz = () => {
+  // show first question
+  setActiveQuestion(currentQuestionIndex)
+  // set  focus to the first question
+  // ---------CONTINUE NEXT WEEK
+}
+
+
+// start quiz when start button is selected
+startQuizButton.addEventListener('click', startQuiz)
+
+
+
+
+
+
+
+// forget the code below for now........
+// // event listener to handle user input for each qestion
+// document.addEventListener('keydown', (event) => {
+//   const currentQuestion = questions[currentQuestionIndex]
+//   const submitButton = currentQuestion.querySelector('.submit-button')
+
+//   // let the user select option by pressing a,b, or c on keyboard
+//   switch (event.key) {
+//     case 'a':
+//       event.preventDefault()
+//       currentQuestion.querySelector('input[value="a"]').checked = true
+//       submitButton.focus()
+//       break
+//     case 'b':
+//       event.preventDefault()
+//       currentQuestion.querySelector('input[value="b"]').checked = true
+//       submitButton.focus()
+//       break
+//     case 'c':
+//       event.preventDefault()
+//       currentQuestion.querySelector('input[value="c"]').checked = true
+//       submitButton.focus()
+//       break
+//   }
+
+//   // if submit button is clicked / or keyboard press enter - go to next question
+//   submitButton.addEventListener('keydown', (event) => {
+//     if (event.key === 'Enter') {
+//       const nextQuestionIndex = (currentQuestionIndex + 1) % questions.length
+//       console.log(nextQuestionIndex)
+//       setActiveQuestion(nextQuestionIndex)
+//     }
+
+//   })
+// })
+
